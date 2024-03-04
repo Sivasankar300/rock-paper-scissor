daf48ca51c72bc6a35916d857325e1bce74bd7e7
SivasankarH <155282396+SivasankarH@users.noreply.github.com>
2024-03-04 22:18:04 +0530

    Realised that I didn't know the purpose of the parameters in "playRound()" so fixed it accordingly and added a UI

--- index.js
+++ index.js
@@ -5,12 +5,54 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-function getPlayerChoice() {
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+let result = document.querySelector("#results");
+const body = document.querySelector("body");
+const reset = document.querySelector('#reset');
+
+rock.addEventListener("click", () => {
+  playRound(1, getComputerChoice());
+  let scores = `Player's score: ${playerScore} Computer's score ${computerScore} Draws: ${draw}`;
+  result.textContent = scores;
+  if(playerScore === 5){
+    result.textContent = "The winner is the Player!"
+  } else if(computerScore === 5){
+    result.textContent = "The winner is the Computer!"
+  }
+});
+
+paper.addEventListener("click", () => {
+  playRound(2, getComputerChoice());
   
+  let scores = `Player's score: ${playerScore} Computer's score ${computerScore} Draws: ${draw}`;
+  result.textContent = scores;
+  if(playerScore === 5){
+    result.textContent = "The winner is the Player!"
+  } else if(computerScore === 5){
+    result.textContent = "The winner is the Computer!"
+
+   }
+});
+scissors.addEventListener("click", () => {
+  playRound(3, getComputerChoice());
+  let scores = `Player's score: ${playerScore} Computer's score ${computerScore} Draws: ${draw}`;
+  result.textContent = scores;
+  if(playerScore === 5){
+    result.textContent = "The winner is the Player!"
+  } else if(computerScore === 5){
+    result.textContent = "The winner is the Computer!"
   
 
-  
-}
+  result.textContent = scores; }
+});
+
+reset.addEventListener('click',() => {
+  playerScore = 0;
+  computerScore = 0;
+  draw = 0;
+})
 
 function playRound(playerSelection, computerSelection) {
   // Compare the strings from player and computer
@@ -20,34 +62,45 @@ function playRound(playerSelection, computerSelection) {
     case 1:
       if (computerSelection === 1) {
         draw++;
+        round++;
         return "Draw";
       } else if (computerSelection === 2) {
         computerScore++;
+        round++;
+
         return "You Lose";
       } else {
         playerScore++;
+        round++;
         return "You Win";
       }
     case 2:
       if (computerSelection === 2) {
         draw++;
+        round++;
         return "Draw";
+        
       } else if (computerSelection === 1) {
         playerScore++;
+        round++;
         return "You win";
       } else {
         computerScore++;
+        round++;
         return "You lose";
       }
     case 3:
       if (computerSelection === 3) {
         draw++;
+        round++;
         return "Draw";
       } else if (computerSelection === 2) {
         playerScore++;
+        round++;
         return "You win";
       } else {
         computerScore++;
+        round++;
         return "You lose";
       }
   }
@@ -56,15 +109,4 @@ function playRound(playerSelection, computerSelection) {
 let playerScore = 0;
 let computerScore = 0;
 let draw = 0;
-
-function playGame() {
-  let rounds = 5;
-  for (i = 0; i < rounds; i++) {
-    playRound(getPlayerChoice(), getComputerChoice());
-  }
-  console.log("Player Score is:", playerScore);
-  console.log("Computer Score is", computerScore);
-  console.log("Draw Score is", draw);
-}
-
-playGame();
+let round = 0;
